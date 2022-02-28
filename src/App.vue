d83a4bbaca33e2a29206b87addf738e21ee6d29d
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-02-28 21:03:45 +0200

    weather

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <WeatherVue />
+</template>
+
+<script>
+import WeatherVue from "./components/Weather.vue"
+export default {
+  name: 'App',
+  components: {
+    WeatherVue
+  }
+}
+</script>
+
+<style>
+
+</style>
